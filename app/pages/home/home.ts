51ecba7a2f81f44b9843929b0d69c8abbc841620
Ali Churcher <churcher.ali@gmail.com>
2016-09-07 22:35:47 +0100

    User can request a movie rating from OimdbApi
    Playing with tutorial from http://gonehybrid.com/build-your-first-mobile-app-with-ionic-2-angular-2-part-5/

---
+++ app/pages/home/home.ts
@@ -0,0 +1,24 @@
+import {Component} from "@angular/core";
+import {GitHubService} from '../../services/github';
+
+@Component({
+    templateUrl: 'build/pages/home/home.html',
+    providers: [GitHubService]
+})
+export class HomePage {
+    public foundRepos;
+    public username;
+
+    constructor(private github: GitHubService) {
+    }
+
+    getRepos() {
+        this.github.getRepos(this.username).subscribe(
+            data => {
+                this.foundRepos = data.json();
+            },
+            err => console.error(err),
+            () => console.log('getRepos completed')
+        );
+    }
+}
---
+++ app/pages/info/home.ts
@@ -0,0 +1,30 @@
+import {Component} from "@angular/core";
+import {OmdbService} from '../../services/omdb';
+
+@Component({
+    templateUrl: 'build/pages/info/home.html',
+    providers: [OmdbService]
+})
+export class HomePage {
+    public title;
+    public foundMovies;
+    public movieTitle;
+    public movieRating;
+
+    constructor(private omdb: OmdbService) {
+    }
+
+    getMovies() {
+      console.log('about to try get some movies')
+      this.omdb.getMovies(this.title).subscribe(
+        data => {
+        this.foundMovies = data.json();
+        this.movieTitle = this.foundMovies.Title;
+        this.movieRating = this.foundMovies.imdbRating;
+      },
+        err => console.error(err),
+        () => console.log('getMovies completed')
+    );
+
+    }
+}
